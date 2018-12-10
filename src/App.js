8d3b8db7760c06ca92f81a81fa997ef2762b1ae2
Skittle Wan <skittle.wan@oocl.com>
2018-12-11 00:05:02 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Icon } from 'antd';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import WelcomePage from "./component/WelcomePage"
+
+const { Header, Sider, Content } = Layout;
+
+
 
 class App extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Layout id="components-layout-demo-custom-trigger" style={{ minHeight: '100vh' }}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>Welcome Page</span>
+              <Link to="/welcomepage">WelcomePage</Link>
+            </Menu.Item>   
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>Table Page</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{
+            margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
+          }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <Switch>
+            <Route path="/welcomepage" component={WelcomePage}></Route>
+            </Switch>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 }
---
+++ src/component/TablePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TablePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Table Page</h1>
+      </div>
+    )
+  }
+}
